6c9bc00fe18d3d91fdeaecb26dfcfc6e7b39c981
zoozoo-code <kyj20908@naver.com>
2022-05-03 22:22:17 +0900

    복습

--- tensorflow/lec-7-5 Fashion MNIST.py
+++ tensorflow/lec-7-5 Fashion MNIST.py
@@ -17,13 +17,13 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-
+# 여기서는 cnn 아니라 그냥 바로 nn에 넣는거라 이럼
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-
+# 최적화 아담
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
