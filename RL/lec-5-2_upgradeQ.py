5368fe71d4e1895f571c9802b1a18e460733aa71
zoozoo-code <kyj20908@naver.com>
2022-05-17 11:01:21 +0900

    강화학습

---
+++ RL/lec-5-2_upgradeQ.py
@@ -0,0 +1,89 @@
+
+import time
+
+import gym
+import numpy as np
+
+
+
+N_ACTIONS = 4
+N_STATES = 16
+
+LEARNING_RATE = .5
+DISCOUNT_RATE = .98
+
+N_EPISODES = 2000
+
+def main():
+    """Main"""
+    frozone_lake_env = gym.make("FrozenLake-v1")
+
+    # Initialize table with all zeros
+    Q = np.zeros([N_STATES, N_ACTIONS])
+
+    # Set learning parameters
+
+    # create lists to contain total rewards and steps per episode
+    rewards = []
+
+    for i in range(N_EPISODES):
+        # Reset environment and get first new observation
+        state = frozone_lake_env.reset()
+        episode_reward = 0
+        done = False
+
+        # The Q-Table learning algorithm
+        while not done:
+            # Choose an action by greedily (with noise) picking from Q table
+            noise = np.random.randn(1, N_ACTIONS) / (i + 1)
+            action = np.argmax(Q[state, :] + noise)
+
+            # Get new state and reward from environment
+            new_state, reward, done, _ = frozone_lake_env.step(action)
+
+            reward = -1 if done and reward < 1 else reward
+
+            # Update Q-Table with new knowledge using learning rate
+            Q[state, action] = (
+                1 - LEARNING_RATE) * Q[state, action] + LEARNING_RATE * (
+                    reward + DISCOUNT_RATE * np.max(Q[new_state, :]))
+
+            episode_reward += reward
+            state = new_state
+
+        rewards.append(episode_reward)
+
+    print("Score over time: " + str(sum(rewards) / N_EPISODES))
+    print("Final Q-Table Values")
+
+    for i in range(10):
+        # Reset environment and get first new observation
+        state = frozone_lake_env.reset()
+        episode_reward = 0
+        done = False
+
+        # The Q-Table learning algorithm
+        while not done:
+            # Choose an action by greedily (with noise) picking from Q table
+            action = np.argmax(Q[state, :])
+
+            # Get new state and reward from environment
+            new_state, reward, done, _ = frozone_lake_env.step(action)
+
+            frozone_lake_env.render()
+            time.sleep(.1)
+
+            episode_reward += reward
+            state = new_state
+
+            if done:
+                print("Episode Reward: {}".format(episode_reward))
+        
+
+        rewards.append(episode_reward)
+
+    frozone_lake_env.close()
+
+
+if __name__ == '__main__':
+    main()
