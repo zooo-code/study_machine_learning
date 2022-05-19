73802d7c52965318e962318c272f4e1c0d32c246
zoozoo-code <kyj20908@naver.com>
2022-05-19 23:21:18 +0900

    Update lec-6-1-Q_network.py

--- RL/lec-6-1-Q_network.py
+++ RL/lec-6-1-Q_network.py
@@ -9,87 +9,54 @@ env = gym.make('FrozenLake-v1')
 input_size = env.observation_space.n
 output_size = env.action_space.n
 learning_rate = 0.1
-
-# These lines establish the feed-forward part of the network used to
-# choose actions
-
-def dense(x,W):
-    return tf.matmul(x,W)
-
-@tf.function
-def model(x,W):
-    y = dense(x,W)
-    return y
-
-
-def loss_fn(x, Y):
-    hypothesis = dense(x,W)
-    cost =  tf.reduce_sum(tf.square(Y - hypothesis))
-    return cost
-
-def grad(x, Y):
-    with tf.GradientTape() as tape:
-        loss_value = loss_fn(x, Y)
-    # 미분값 반환
-    return tape.gradient(loss_value, [W])
-
-
-W = tf.Variable(tf.random.uniform([input_size, output_size], 0, 0.01),name = 'weight')  # weight
-
-Y = tf.Variable(shape=[1, output_size], dtype=tf.float32)  # Y label
-
-optimizer = tf.keras.optimizers.SGD(learning_rate = learning_rate)
-
 # Set Q-learning related parameters
 
 dis = .99
 num_episodes = 2000
 
-# Create lists to contain total rewards and steps per episode
-rList = []
-
 def one_hot(x):
     return np.identity(16)[x:x + 1]
 
+# These lines establish the feed-forward part of the network used to
+# choose actions
 
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(output_size, input_shape=[input_size],
+                         kernel_initializer=tf.random_uniform_initializer(minval=0, maxval=0.01))
+])
+
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate= learning_rate), loss='mse')
+
+model.summary()
+
+rList=[]
 for i in range(num_episodes):
-    # Reset environment and get first new observation
     s = env.reset()
-    e = 1. / ((i / 50) + 10)
+    e = 1.0 / ((i/50)+10)
     rAll = 0
     done = False
     local_loss = []
 
-    # The Q-Network training
     while not done:
-        # Choose an action by greedily (with e chance of random action)
-        # from the Q-network
-        Qs = sess.run(Qpred, feed_dict={X: one_hot(s)})
+        Qs = model.predict(one_hot(s))
         if np.random.rand(1) < e:
             a = env.action_space.sample()
         else:
             a = np.argmax(Qs)
 
-        # Get new state and reward from environment
         s1, reward, done, _ = env.step(a)
         if done:
-            # Update Q, and no Qs+1, since it's a terminal state
             Qs[0, a] = reward
         else:
-            # Obtain the Q_s1 values by feeding the new state through our
-            # network
-            Qs1 = sess.run(Qpred, feed_dict={X: one_hot(s1)})
-            # Update Q
-            Qs[0, a] = reward + dis * np.max(Qs1)
+            Qs1 = model.predict(one_hot(s1))
+            Qs[0, a] = reward + dis*np.max(Qs1)
 
-        # Train our network using target (Y) and predicted Q (Qpred) values
-        sess.run(train, feed_dict={X: one_hot(s), Y: Qs})
+        model.fit(x=one_hot(s), y=Qs )
 
         rAll += reward
-        s = s1
+        s= s1
     rList.append(rAll)
 
-print("Percent of successful episodes: " +
-      str(sum(rList) / num_episodes) + "%")
-plt.bar(range(len(rList)), rList, color="blue")
+print("Percent of successful episode: "+str(sum(rList)/num_episodes)+"%")
+plt.bar(range(len(rList)), rList, color='blue')
 plt.show()
